92fe2b6f8484df9fbda4371104e2605ce3e3b172
Philip Thruesen <philip@thruesen.dk>
2016-03-23 13:33:07 +0100

    ground truth script added

---
+++ GroundTruthExtractor/GroundTruthExtractor.py
@@ -0,0 +1,7 @@
+import numpy as np;
+
+with open(r'C:\Users\phili_000\Desktop\MI8 data\2015_01_clickstream.tsv\2015_01_clickstream.tsv') as f:
+    for line in f:
+        print(line)
+        lineSplit = line.split('\t')
+
