238e6f81887014a6f95fb618fa6369c1fe4a4d11
Philip Thruesen <philip@thruesen.dk>
2016-04-06 15:08:33 +0200

    prepared extraction for 2016 data files

--- GroundTruthExtractor/GroundTruthExtractor.py
+++ GroundTruthExtractor/GroundTruthExtractor.py
@@ -7,9 +7,9 @@ import os
 testingOnPhilipsPC = os.name == 'nt'
 
 # set file path to clickstream file
-clickstreamFile = r'/data/2015_02_clickstream.tsv'
+clickstreamFile = r'/data/2013_03_clickstream.tsv'
 if testingOnPhilipsPC:
-    clickstreamFile = r'C:\Users\phili_000\Desktop\MI8 data\2015_02\2015_2_clickstream.tsv'
+    clickstreamFile = r'C:\Users\phili_000\Desktop\MI8 data\2016_03\2016_03_clickstream.tsv'
 
 # assign variables
 currentArticle = ''
@@ -18,10 +18,16 @@ numberOfTruth = 10
 truthHeap = []
 lineCnt = 0
 
+# each of the clickstream files have different coloumns and therefore we can enumerate them below
+PREV_COL = 0
+CURR_COL = 1
+TYPE_COL = 2
+COUNT_COL = 3
+
 # loop through clickstream file to extract the most "important" articles
 with open(clickstreamFile) as f:
     for line in f:
-
+        
         # increment line counter
         lineCnt += 1
 
@@ -34,25 +40,25 @@ with open(clickstreamFile) as f:
 
         # if the currentArticle "pointer" has not been set then set it to current article
         if not currentArticle: 
-            currentArticle = int(lineSplit[1])
+            currentArticle = lineSplit[CURR_COL]
 
         # if the row represents a link to a non-existing article then ignore tuple
-        if not lineSplit[1] or "redlink" in lineSplit[5]:
+        if not lineSplit[CURR_COL]:
             continue
         
         # aggregate over the number of outgoing trails from an article and store the articles that redirects most users to other pages.
         # articles are remembered using a homebaked max-heap storing the most interesting "numberOfTruth"-articles
-        if currentArticle == int(lineSplit[1]):
-            sessionSum += int(lineSplit[2])
+        if currentArticle == lineSplit[CURR_COL]:
+            sessionSum += int(lineSplit[COUNT_COL])
         else:
             if len(truthHeap) < numberOfTruth or truthHeap[-1][0] < sessionSum:
-                truthHeap.append([sessionSum, int(lineSplit[1])])
+                truthHeap.append([sessionSum, lineSplit[CURR_COL]])
                 if len(truthHeap) > numberOfTruth:
                     truthHeap = sorted(truthHeap, key=lambda truthHeap_entry: truthHeap_entry[0], reverse = True) 
                     truthHeap.pop();
-                    break
-            currentArticle = int(lineSplit[1])
-            sessionSum = int(lineSplit[2])
+                    break # TODO: remove line
+            currentArticle = lineSplit[CURR_COL]
+            sessionSum = int(lineSplit[COUNT_COL])
 
 # if we have extracted data for less than 100 articles its safe to print to console        
 if numberOfTruth < 100:
@@ -80,8 +86,8 @@ with open(clickstreamFile) as f:
         
         # if the prev article is in the set of important articles then:
             # add the curr:number to ground truth file at its respective line
-        if lineSplit[0]:
-            match = [item for item in truthHeap if item[1] == int(lineSplit[1])]
+        if lineSplit[PREV_COL]:
+            match = [item for item in truthHeap if item[1] == lineSplit[CURR_COL]]
             if match:
                 index = truthHeap.index(match[0])
                 fooo = 1
